6bf04ca5ea785bad6452764df04574a342f1776d
Smey <heme-sodexs-devthree>
2024-07-10 15:18:11 +0700

    raksmey

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -42,8 +42,7 @@ const Detail = () => {
   const detailData = details[detail];
 
   const shareUrl = window.location.href;
-  const title = detailData.title;
-  const description = detailData.description;
+
 
   return (
     <>
@@ -52,13 +51,13 @@ const Detail = () => {
         <title>{title}</title>
         <meta property="og:type" content="website" />
         <meta property="og:url" content={shareUrl} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
+        <meta property="og:title" content={detailData.title} />
+        <meta property="og:description" content={detailData.description} />
         <meta property="og:image" content={detailData.image} /> 
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={shareUrl} />
-        <meta property="twitter:title" content={title} />
+        <meta property="twitter:title" content={detailData.title} />
         <meta property="twitter:description" content={description} />
         <meta property="twitter:image" content={detailData.image} />
 
