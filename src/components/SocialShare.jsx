967e66922f1682ce40416ffd428f5ea9240db812
Smey <heme-sodexs-devthree>
2024-07-10 10:38:24 +0700

    raksmey

--- src/components/SocialShare.jsx
+++ src/components/SocialShare.jsx
@@ -32,21 +32,6 @@ const SocialShare = ({ world }) => {
         padding: "20px",
       }}
     >
-      <Helmet>
-        <title>{title}</title>
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={shareUrl} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={shareUrl} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={image} />
-      </Helmet>
       <Box
         sx={{
           display: "flex",
