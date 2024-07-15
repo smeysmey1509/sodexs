8e25b69f3d14fd48493b0c70546433838f1e90b3
Smey <heme-sodexs-devthree>
2024-07-15 16:28:34 +0700

    update

--- src/components/SocialShare.jsx
+++ src/components/SocialShare.jsx
@@ -10,6 +10,7 @@ import {
   EmailShareButton,
   EmailIcon,
 } from "react-share";
+import { Helmet } from "react-helmet";
 
 const SocialShare = ({
   shareUrl,
@@ -41,16 +42,22 @@ const SocialShare = ({
           url={shareUrl}
           quote={shareQuote}
           hashtag={shareHashtag}
+          data-testid="facebook-share-button"
         >
           <FacebookIcon size={32} round />
         </FacebookShareButton>
-        <TwitterShareButton url={shareUrl} title={shareTitle}>
+        <TwitterShareButton
+          url={shareUrl}
+          title={shareTitle}
+          data-testid="twitter-share-button"
+        >
           <TwitterIcon size={32} round />
         </TwitterShareButton>
         <LinkedinShareButton
           url={shareUrl}
           title={shareTitle}
           summary={shareDescription}
+          data-testid="linkedin-share-button"
         >
           <LinkedinIcon size={32} round />
         </LinkedinShareButton>
@@ -58,6 +65,7 @@ const SocialShare = ({
           url={shareUrl}
           subject={shareTitle}
           description={shareDescription}
+          data-testid="email-share-button"
         >
           <EmailIcon size={32} round />
         </EmailShareButton>
@@ -67,3 +75,33 @@ const SocialShare = ({
 };
 
 export default SocialShare;
+
+export const Metadata = ({
+  mainTitle,
+  shareUrl,
+  shareTitle,
+  shareDescription,
+  shareImage,
+}) => {
+  return (
+    <Helmet>
+      <title>{mainTitle}</title>
+
+      {/* Facebook */}
+      <meta property="og:type" content="Webpage" />
+      <meta property="og:url" content={shareUrl} />
+      <meta property="og:title" content={shareTitle} />
+      <meta property="og:description" content={shareDescription} />
+      <meta property="og:image" content={shareImage} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="Card" />
+      <meta property="twitter:url" content={shareUrl} />
+      <meta property="twitter:title" content={shareTitle} />
+      <meta property="twitter:description" content={shareDescription} />
+      <meta property="twitter:image" content={shareImage} />
+    </Helmet>
+  );
+};
--- src/unit-test/socialshare/SocialShare.test.js
+++ src/unit-test/socialshare/SocialShare.test.js
@@ -1,48 +1,30 @@
-// SocialShare.test.js
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import SocialShare from "../../components/SocialShare";
 
-describe("SocialShare", () => {
-  test("renders social share buttons with props", async () => {
-    const mockProps = {
-      shareUrl: "https://example.com",
-      shareQuote: "Check out this link!",
-      shareHashtag: "#example",
-      shareTitle: "Example Title",
-      shareDescription: "Description of the share",
-      shareImage: "https://example.com/image.jpg",
-    };
+describe("SocialShare component", () => {
+  const mockProps = {
+    shareUrl: "https://example.com",
+    shareQuote: "Check out this example!",
+    shareHashtag: "#example",
+    shareTitle: "Example Title",
+    shareDescription: "This is an example description.",
+    shareImage: "https://example.com/image.jpg",
+  };
 
-    render(
-      <SocialShare
-        shareUrl={mockProps.shareUrl}
-        shareQuote={mockProps.shareQuote}
-        shareHashtag={mockProps.shareHashtag}
-        shareTitle={mockProps.shareTitle}
-        shareDescription={mockProps.shareDescription}
-        shareImage={mockProps.shareImage}
-      />
-    );
+  it("renders social share buttons", () => {
+    const { getByTestId } = render(<SocialShare {...mockProps} />);
 
-    await waitFor(() => {
-      const facebookButtons = screen.getAllByRole("button", {
-        name: /facebook/gsv,
-      });
-      expect(facebookButtons.length).toBeGreaterThan(0);
+    const facebookButton = getByTestId("facebook-share-button");
+    expect(facebookButton).toBeInTheDocument();
 
-      const twitterButtons = screen.getAllByRole("button", {
-        name: /twitter/gsv,
-      });
-      expect(twitterButtons.length).toBeGreaterThan(0);
+    const twitterButton = getByTestId("twitter-share-button");
+    expect(twitterButton).toBeInTheDocument();
 
-      const linkedinButtons = screen.getAllByRole("button", {
-        name: /linkedin/gsv,
-      });
-      expect(linkedinButtons.length).toBeGreaterThan(0);
+    const linkedinButton = getByTestId("linkedin-share-button");
+    expect(linkedinButton).toBeInTheDocument();
 
-      const emailButtons = screen.getAllByRole("button", { name: /email/gsv });
-      expect(emailButtons.length).toBeGreaterThan(0);
-    });
+    const emailButton = getByTestId("email-share-button");
+    expect(emailButton).toBeInTheDocument();
   });
 });
