a909ad89a9f75543f0c01f6a5c4b60db8ea2a4c7
Smey <heme-sodexs-devthree>
2024-07-16 10:47:39 +0700

    update

---
+++ src/components/KlebJeb.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const KlebJeb = () => {
+  function sum(a, b) {
+    return a + b;
+  }
+
+  return <div>{sum(5, 10)}</div>;
+};
+
+export default KlebJeb;
--- src/unit-test/socialshare/SocialShare.test.js
+++ src/unit-test/socialshare/SocialShare.test.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import SocialShare from "../../components/SocialShare";
+import SocialShare, { Metadata } from "../../components/SocialShare";
+import { Helmet } from "react-helmet";
+import { HelmetProvider } from "react-helmet-async";
 
-describe("SocialShare component", () => {
+describe("SocialShare", () => {
   const mockProps = {
     shareUrl: "https://example.com",
     shareQuote: "Check out this example!",
@@ -26,5 +28,77 @@ describe("SocialShare component", () => {
 
     const emailButton = getByTestId("email-share-button");
     expect(emailButton).toBeInTheDocument();
+  });
+});
+
+describe("Metadata", () => {
+  const mockProps = {
+    mainTitle: "Example Page",
+    shareUrl: "https://example.com",
+    shareTitle: "Example Title",
+    shareDescription: "This is an example description.",
+    shareImage: "https://example.com/image.jpg",
+  };
+
+  it("renders metadata tags for Open Graph and Twitter", () => {
+    render(
+      <HelmetProvider>
+        <Metadata {...mockProps} />
+      </HelmetProvider>
+    );
+
+    const helmet = Helmet.peek();
+
+    // Check Open Graph tags
+    expect(helmet.metaTags).toContainEqual({
+      property: "og:type",
+      content: "Webpage",
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "og:url",
+      content: mockProps.shareUrl,
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "og:title",
+      content: mockProps.shareTitle,
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "og:description",
+      content: mockProps.shareDescription,
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "og:image",
+      content: mockProps.shareImage,
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "og:image:width",
+      content: "1200",
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "og:image:height",
+      content: "630",
+    });
+
+    // Check Twitter tags
+    expect(helmet.metaTags).toContainEqual({
+      property: "twitter:card",
+      content: "Card",
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "twitter:url",
+      content: mockProps.shareUrl,
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "twitter:title",
+      content: mockProps.shareTitle,
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "twitter:description",
+      content: mockProps.shareDescription,
+    });
+    expect(helmet.metaTags).toContainEqual({
+      property: "twitter:image",
+      content: mockProps.shareImage,
+    });
   });
 });
