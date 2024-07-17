5ba2144221326ca6097ea1f7b8d67cd009057312
Smey <heme-sodexs-devthree>
2024-07-17 14:43:46 +0700

    update

--- src/unit-test/socialshare/KlebJeb.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import KlebJeb from "../../components/KlebJeb";
-
-test("KlebJeb", () => {
-  const { getByText } = render(<KlebJeb />);
-  const klebJebElement = getByText(15);
-  expect(klebJebElement).toBeInTheDocument();
-});
